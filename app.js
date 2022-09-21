d5828945903be6a0ca70641ea7174cc1fa9195e2
Juan Gamonoso <juan.gamonoso@stratesys-ts.com>
2022-09-21 17:33:53 +0200

    task 31

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 console.log(Vue)
 
 const app = Vue.createApp({
-    template: `
-    <h1>Hola mundo</h1>
-    <p>Desde app.js</p>
-    `
+    // template: `
+    // <h1>Hola mundo</h1>
+    // <p>Desde app.js</p>
+    // <p> {{ true ? 'Activo' : 'Inactivo' }} </p>
+    // `
 })
 
 app.mount('#myApp')
